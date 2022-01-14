e340612c0f1128d3c5fd0a88561e4b4a1ecf3681
Oghenekparobo Onosemuode <kparaboj@gmail.com>
2022-01-15 00:40:01 +0100

    hero section modified & modal backdrop added

--- assets/scripts/modal.js
+++ assets/scripts/modal.js
@@ -1,6 +1,7 @@
 var modal = document.querySelector('#modal');
 var modalButtons = document.querySelectorAll('.toggle-modal');
 var lastFocussedElement;
+var body = document.getElementById("top");
 
 function toggleModal() {
   if (modal.style.display == 'block') {
@@ -12,6 +13,7 @@ function toggleModal() {
 
 function openModal() {
   modal.style.display = 'block';
+  body.classList += "bg-modal";
 
   // Remember the last selected item on the page
   lastFocussedElement = document.activeElement;
@@ -23,6 +25,7 @@ function openModal() {
 
 function closeModal() {
   modal.style.display = 'none';
+  body.classList = "";
 
   // Return the user to the last item they were looking at before opening the modal
   lastFocussedElement.focus();
@@ -36,3 +39,13 @@ document.addEventListener('keydown', (event) => {
     closeModal();
   }
 });
+
+// Allow the user to close the modal by clicking outside the modal
+//if(modal.style.display === 'block'){
+//    body.addEventListener('click', (event) => {
+//        var modalClicked = modal.contains(event.target);
+//        if(!modalClicked){
+//            console.log('clicked')
+//        }
+//    });
+//}
