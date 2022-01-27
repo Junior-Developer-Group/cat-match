1daa2dc42eba7c3798003cb234d45b8cf8ad2f66
codekoya <angelanikolova218@gmail.com>
2022-01-27 13:43:18 +0100

    Added code for pre-loader section

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,11 +3,11 @@ const allcats=document.getElementById("allcats");
 const submitButton=document.querySelector(".submitButton");
 const form=document.getElementById("form");
 const allCatsButton=document.querySelector(".allCatsButton");
+const loader = document.querySelector(".loader");
 let catBreeds; 
 
-
-
 submitButton.addEventListener("click", async (e) => {
+    displayLoader();
     e.preventDefault();
     getUserInputs();
     console.log(userValues);
@@ -16,9 +16,20 @@ submitButton.addEventListener("click", async (e) => {
     if(result){
         console.log(result.data);
         catBreeds = result.data;
+        //the hideLoader function should go here, when the code for displaying the cats data is done
         //Code for displaying the cats based on the input search parameters goes here.
     }
 });
+function displayLoader(){
+    loader.classList.add("display");
+    setTimeout(() => {
+        loader.classList.remove("display");
+    }, 5000);
+}
+
+function hideLoader(){ //use function when api is done fetching data
+    loader.classList.remove("display");
+}
 
 function getUserInputs() {
     const inputs=form.elements;
