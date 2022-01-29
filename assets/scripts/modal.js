1ea5c5ad125dd293674a7a86b95c2f2a1e57d211
Oghenekparobo Onosemuode <kparaboj@gmail.com>
2022-01-29 16:51:38 +0100

    revert changes

--- assets/scripts/modal.js
+++ assets/scripts/modal.js
@@ -1,7 +1,6 @@
 var modal = document.querySelector('#modal');
 var modalButtons = document.querySelectorAll('.toggle-modal');
 var lastFocussedElement;
-var body = document.getElementById("top");
 
 function toggleModal() {
   if (modal.style.display == 'block') {
@@ -13,7 +12,6 @@ function toggleModal() {
 
 function openModal() {
   modal.style.display = 'block';
-  body.classList += "bg-modal";
 
   // Remember the last selected item on the page
   lastFocussedElement = document.activeElement;
@@ -25,7 +23,6 @@ function openModal() {
 
 function closeModal() {
   modal.style.display = 'none';
-  body.classList = "";
 
   // Return the user to the last item they were looking at before opening the modal
   lastFocussedElement.focus();
@@ -39,13 +36,3 @@ document.addEventListener('keydown', (event) => {
     closeModal();
   }
 });
-
-// Allow the user to close the modal by clicking outside the modal
-//if(modal.style.display === 'block'){
-//    body.addEventListener('click', (event) => {
-//        var modalClicked = modal.contains(event.target);
-//        if(!modalClicked){
-//            console.log('clicked')
-//        }
-//    });
-//}
