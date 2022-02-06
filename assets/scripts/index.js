809d026367e36969bcae72cdd97e48b03d4d3bd1
Kyle-Atienza <atienzakylefrancis@gmail.com>
2022-02-06 11:10:17 +0800

    initial

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,47 +1,52 @@
-const catApi="https://api.thecatapi.com/v1/breeds";
-const allcats=document.getElementById("allcats");
-const submitButton=document.querySelector(".submitButton");
-const form=document.getElementById("form");
-const allCatsButton=document.querySelector(".allCatsButton");
+const catApi = "https://api.thecatapi.com/v1/breeds";
+const allcats = document.getElementById("allcats");
+const submitButton = document.querySelector(".submitButton");
+const form = document.getElementById("form");
+const allCatsButton = document.querySelector(".allCatsButton");
+
 const loader = document.querySelector(".loader");
-let catBreeds; 
+let catResults = [];
+let catBreeds;
 
 submitButton.addEventListener("click", async (e) => {
-    displayLoader();
-    e.preventDefault();
-    getUserInputs();
-    console.log(userValues);
+  displayLoader();
+  e.preventDefault();
+  getUserInputs();
+  console.log(userValues);
 
-    let result = await getAllCatBreeds();
-    if(result){
-        console.log(result.data);
-        catBreeds = result.data;
-        //the hideLoader function should go here, when the code for displaying the cats data is done
-        //Code for displaying the cats based on the input search parameters goes here.
-    }
+  let result = await getAllCatBreeds();
+  if (result) {
+    console.log(result.data);
+    catBreeds = result.data;
+    //the hideLoader function should go here, when the code for displaying the cats data is done
+    //Code for displaying the cats based on the input search parameters goes here.
+  }
+
+  let query = [];
+  console.log(userValues);
+  console.log(catResults);
 });
-function displayLoader(){
-    loader.classList.add("display");
-    setTimeout(() => {
-        loader.classList.remove("display");
-    }, 5000);
+function displayLoader() {
+  loader.classList.add("display");
+  setTimeout(() => {
+    loader.classList.remove("display");
+  }, 5000);
 }
 
-function hideLoader(){ //use function when api is done fetching data
-    loader.classList.remove("display");
+function hideLoader() {
+  //use function when api is done fetching data
+  loader.classList.remove("display");
 }
 
 function getUserInputs() {
-    const inputs=form.elements;
-    let formValues={};
-    for (const input of inputs) {
-        if (input.type=="checkbox") {
-            formValues[input.id]=input.checked==true? 1:0;
-        } else {
-            formValues[input.id]=input.value;
-        }
+  const inputs = form.elements;
+  let formValues = {};
+  for (const input of inputs) {
+    if (input.type == "checkbox") {
+      formValues[input.id] = input.checked == true ? 1 : 0;
+    } else {
+      formValues[input.id] = input.value;
     }
-    return userValues={ ...formValues };
-};
-
-
+  }
+  return (userValues = { ...formValues });
+}
