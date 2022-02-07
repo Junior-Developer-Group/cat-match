6456362290bcab3b196f99de1ab2bdca4496d085
Kyle-Atienza <atienzakylefrancis@gmail.com>
2022-02-07 13:21:37 +0800

    resolved conflicts

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,15 +3,20 @@ const allcats = document.getElementById("allcats");
 const submitButton = document.querySelector(".submitButton");
 const form = document.getElementById("form");
 const allCatsButton = document.querySelector(".allCatsButton");
+<<<<<<< HEAD
 
 const loader = document.querySelector(".loader");
 let catResults;
+=======
+const loader = document.querySelector(".loader");
+>>>>>>> ee1ce78 (add logic for reseting search form)
 let catBreeds;
 
 submitButton.addEventListener("click", async (e) => {
   displayLoader();
   e.preventDefault();
   getUserInputs();
+<<<<<<< HEAD
 
   let result = await getAllCatBreeds();
   if (result) {
@@ -39,6 +44,17 @@ submitButton.addEventListener("click", async (e) => {
   }
   console.log(catBreeds);
   console.log(catResults);
+=======
+  console.log(userValues);
+
+  let result = await getAllCatBreeds();
+  if (result) {
+    console.log(result.data);
+    catBreeds = result.data;
+    //the hideLoader function should go here, when the code for displaying the cats data is done
+    //Code for displaying the cats based on the input search parameters goes here.
+  }
+>>>>>>> ee1ce78 (add logic for reseting search form)
 });
 function displayLoader() {
   loader.classList.add("display");
@@ -59,8 +75,31 @@ function getUserInputs() {
     if (input.type == "checkbox") {
       formValues[input.id] = input.checked == true ? 1 : 0;
     } else if (input.type == "select-one") {
+<<<<<<< HEAD
       formValues[input.id] = parseInt(input.value);
     }
   }
   return (userValues = { ...formValues });
 }
+=======
+      formValues[input.id] = input.value;
+    }
+  }
+  //this may be changes to a button click?
+  resetSearchForm();
+  return (userValues = { ...formValues });
+}
+
+function resetSearchForm() {
+  const inputs = form.elements;
+  for (const input of inputs) {
+    if (input.type == "checkbox" || input.type == "select-one") {
+      if (input.type == "checkbox") {
+        input.checked = false;
+      } else if (input.type == "select-one") {
+        input.value = "";
+      }
+    }
+  }
+}
+>>>>>>> ee1ce78 (add logic for reseting search form)
